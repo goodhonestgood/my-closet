3e03bcd042ad7c0bd7b61bc911af0c138d6bbcd4
goodhonestgood <tktk517@naver.com>
2022-04-23 23:54:28 +0900

    이미지 자르기 하는 중

--- my-closet-project/src/views/Test.vue
+++ my-closet-project/src/views/Test.vue
@@ -5,7 +5,7 @@
             <input type="file" @change="getImage">
         </div>
         <div v-else>
-            <img :src="image" class="rounded"/>
+            <canvas ref="canvas" width="500px" height="500px"></canvas>
             <button @click="remove">Remove image</button>
         </div>
         <h5>2. 정사각형 자르기</h5>
@@ -18,26 +18,37 @@
 import { ref } from 'vue'
 export default {
     setup() {
-        const image = ref(null)
+        const canvas = ref(null)
+        // 이미지 가져오기
         const getImage = (event) => {
             let file = event.target.files || event.dataTransfer.files
             if (!file) return
-            let img = new Image()
             let reader = new FileReader()
             reader.onload = (e) => {
-                image.value = e.target.result
+                reSize(e.target.result)
             }
             reader.readAsDataURL(file[0])
         }
+        // 이미지 리사이즈
+        const reSize = (result) => {
+            
+            console.log()
+            /*
+            const ctx = canvas.getContext('2d')
+            let image = new Image()
+            image.src = result
+            image.onload = () => {
+                ctx.drawImage(image, 150, 200, 500, 300, 60,60, 500, 500)
+            }*/
+        }
+        
+        // 이미지 지우기
         const remove = () => {
             image.value = null
         }
         // 여기부터
-        const resize = () => {
 
-        }
-
-        return {getImage, image, remove}
+        return {getImage, remove, canvas}
     },
 }
 </script>
