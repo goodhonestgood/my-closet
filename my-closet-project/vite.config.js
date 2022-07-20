80128deb205d2dd694a50b2b262df23739fd0944
goodhonestgood <tktk517@naver.com>
2022-07-20 20:58:12 +0900

    deploy3

--- my-closet-project/vite.config.js
+++ my-closet-project/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: '/my-closet',
+  baseURL: '/my-closet',
   outputDir: 'docs',
   devServer: {
     proxy: {
