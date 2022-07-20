29e3288d6015c62374b9f43258164ae3772fff40
goodhonestgood <tktk517@naver.com>
2022-07-20 20:52:12 +0900

    config 수정

--- my-closet-project/vite.config.js
+++ my-closet-project/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publickPath: '/',
+  publicPath: '/',
   outputDir: 'docs',
   devServer: {
     proxy: {
